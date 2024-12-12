5f0fea678c79b68dd39b9b278029c763090675de
Suwit <suwit.saiso@gmail.com>
2024-12-12 19:12:17 +0700

    send-file-json

--- docker-communication/Receiver/app.py
+++ docker-communication/Receiver/app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def ping():
     return jsonify({"message": "ping from {}".format(request.host)})
 
+@app.route('/endpoint', methods=['POST'])
+def receive_json():
+    data = request.get_json()
+    if data:
+        print("Received JSON:", data)
+        return jsonify({"status": "success", "received": data}), 200
+    else:
+        return jsonify({"status": "error", "message": "Invalid JSON"}), 400
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
